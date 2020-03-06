e1bc3c32f35a12b382346c713357cffad9e66587
t-cmath <60342399+t-cmath@users.noreply.github.com>
2020-03-06 11:50:07 +0800

    upload markdown
    绝望的格式 谁帮忙改改组织结构和sidebar呀QAQ

---
+++ org/nixos-manual-zh/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'NixOS 手册',
+    base: '/nix-zh/',
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }]
+    ],
+    themeConfig: {
+        sidebar: 'auto',
+        lastUpdated: 'Last Updated'
+    }
+}
